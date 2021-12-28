a6ed89824c3330f7d89b61009390a1281e44f7a9
Michał Wachowicz <wachowiczmichal1@gmail.com>
2021-12-28 15:14:59 +0100

    Add multiply and divide functions and tests

--- src/calculator.js
+++ src/calculator.js
@@ -17,7 +17,23 @@ export default class Calculator {
     return result;
   }
 
-  multiply(a, b) {}
+  multiply(a, b) {
+    const aNum = parseFloat(a);
+    const bNum = parseFloat(b);
+    const result = aNum * bNum;
 
-  divide(a, b) {}
+    if (isNaN(result)) throw new Error('Error! The result is not a number!');
+    return result;
+  }
+
+  divide(a, b) {
+    const aNum = parseFloat(a);
+    const bNum = parseFloat(b);
+    if (bNum === 0) throw new Error("Error! You can't divide by 0");
+
+    const result = aNum / bNum;
+    if (isNaN(result)) throw new Error('Error! The result is not a number!');
+
+    return result;
+  }
 }
--- test/calculator.test.js
+++ test/calculator.test.js
@@ -55,3 +55,63 @@ describe('Calculator - Subtract', () => {
     );
   });
 });
+
+describe('Calculator - Multiply', () => {
+  test('Multiply two integers', () => {
+    expect(calculator.multiply(2, 4)).toBe(8);
+  });
+
+  test('Multiply two integers with string value', () => {
+    expect(calculator.multiply(2, '2')).toBe(4);
+  });
+
+  test('Multiply two decimal numbers', () => {
+    expect(calculator.multiply(2, 2.5)).toBeCloseTo(5);
+  });
+
+  test('Multiply two decimal numbers with string value', () => {
+    expect(calculator.multiply(2, '3.6')).toBeCloseTo(7.2);
+  });
+
+  test('Multiply by falsy value', () => {
+    expect(() => calculator.multiply(2, false)).toThrow(
+      'Error! The result is not a number!',
+    );
+  });
+
+  test('Multiply two numbers to return NaN', () => {
+    expect(() => calculator.multiply(2, 'test')).toThrow(
+      'Error! The result is not a number!',
+    );
+  });
+});
+
+describe('Calculator - Divide', () => {
+  test('Divide two integers', () => {
+    expect(calculator.divide(4, 2)).toBe(2);
+  });
+
+  test('Divide two integers with string value', () => {
+    expect(calculator.divide(2, '2')).toBe(1);
+  });
+
+  test('Divide two decimal numbers', () => {
+    expect(calculator.divide(5.2, 2)).toBeCloseTo(2.6);
+  });
+
+  test('Divide two decimal numbers with string value', () => {
+    expect(calculator.divide(5, '2.5')).toBeCloseTo(2);
+  });
+
+  test('Divide by 0', () => {
+    expect(() => calculator.divide(2, 0)).toThrow(
+      "Error! You can't divide by 0",
+    );
+  });
+
+  test('Divide two numbers to return NaN', () => {
+    expect(() => calculator.divide(2, 'test')).toThrow(
+      'Error! The result is not a number!',
+    );
+  });
+});
