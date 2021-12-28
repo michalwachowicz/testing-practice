4a0d24fc660a4d90f62e194c5574dd632cfa341f
Michał Wachowicz <wachowiczmichal1@gmail.com>
2021-12-28 14:57:57 +0100

    Add subtract function and tests

--- src/calculator.js
+++ src/calculator.js
@@ -5,10 +5,17 @@ export default class Calculator {
     const result = aNum + bNum;
 
     if (isNaN(result)) throw new Error('Error! The result is not a number!');
-    return aNum + bNum;
+    return result;
   }
 
-  subtract(a, b) {}
+  subtract(a, b) {
+    const aNum = parseFloat(a);
+    const bNum = parseFloat(b);
+    const result = aNum - bNum;
+
+    if (isNaN(result)) throw new Error('Error! The result is not a number!');
+    return result;
+  }
 
   multiply(a, b) {}
 
--- test/calculator.test.js
+++ test/calculator.test.js
@@ -25,3 +25,33 @@ describe('Calculator - Add', () => {
     );
   });
 });
+
+describe('Calculator - Subtract', () => {
+  test('Subtract two integers', () => {
+    expect(calculator.subtract(5, 3)).toBe(2);
+  });
+
+  test('Subtract two integers with string type', () => {
+    expect(calculator.subtract(10, '8')).toBe(2);
+  });
+
+  test('Subtract two decimal numbers', () => {
+    expect(calculator.subtract(0.4, 0.1)).toBeCloseTo(0.3);
+  });
+
+  test('Subtract two decimal numbers with string type', () => {
+    expect(calculator.subtract('1', 0.2)).toBeCloseTo(0.8);
+  });
+
+  test('Subtract number from falsy value', () => {
+    expect(() => calculator.subtract('', 2)).toThrow(
+      'Error! The result is not a number!',
+    );
+  });
+
+  test('Subtract two numbers to return NaN', () => {
+    expect(() => calculator.subtract('text', 2)).toThrow(
+      'Error! The result is not a number!',
+    );
+  });
+});
